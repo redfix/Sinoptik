b248663f85b82197fa5d3cdd443afa64ca19f759
FruityBrew <kompota@yandex.ru>
2015-09-29 18:13:05 +0300

    Класс модели XClient

---
+++ Sinoptik/Model/XClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinoptik.Model
+{
+    class XClient
+    {
+        internal Int32 Id { get; set; }
+        internal String Name {get; set;}
+        internal DateTime DateBirthday { get; set; }
+        internal Boolean IsRheumaticSufferer { get; set; }
+        internal Boolean IsHearhSufferer { get; set; }
+        internal Boolean IsNeuroticSufferer { get; set; }
+    }
+}
