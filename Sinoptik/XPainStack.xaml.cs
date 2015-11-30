0f591eb25b1bcb66cb0173017588e0ec407c3722
redfix <redfx@mail.ru>
2015-11-30 16:28:17 +0300

    pain extend

---
+++ Sinoptik/XPainStack.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sinoptik
+{
+  /// <summary>
+  /// Interaction logic for XPainStack.xaml
+  /// </summary>
+  public partial class XPainStack : UserControl
+  {
+    public XPainStack()
+    {
+      InitializeComponent();
+    }
+  }
+}
